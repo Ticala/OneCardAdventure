5611988a4a86f7090484491b90c80953106a6fc3
Lars Charmer Jørgensen <Lars.charmer@gmail.com>
2022-11-06 13:49:14 +0100

    character methode can attack and more

--- character.py
+++ character.py
@@ -1,41 +1,46 @@
 import math
 
 from point import Point
+
 class Character:
-    def __init__(self, name, life, attack, reach, defends, speed, point ):
+    def __init__(self, name, life, attack, move, defends, range, point ):
         self.name = name
         self.life = life
         self.attack = attack
-        self.reach = reach
+        self.move = move
         self.defends = defends
-        self.speed = speed
+        self.range = range
         self.point = point
 
     def __str__(self):
         return self.name + " has \nlife: " + str(self.life) + \
-         "\nattack " + str(self.attack)
+         "\nattack " + str(self.attack) + \
+         " range   " + str(self.range) + \
+         " defends " + str(self.defends) + \
+         " move    " + str(self.move) + \
+         " place   " + str(self.point)
 
     def canAttack(self, pillars, enemies):
-
-        things = []
-        for pillar in pillars:
-            things.append(pillar)
+        enemiesInRange = []
+        things = pillars + []
         for enemy in enemies:
             things.append(enemy.point)
 
         # is enemy in range
         for enemy in enemies:
-            distance = enemy.point.distance(enemy.point)
-            if distance <= self.range:
+            distance = self.point.distance(enemy.point)
+            if distance > 0 and distance <= self.range:
                 #
-                #  [ ][ ][ ]   [ ][ ][H]
-                #  [X][ ][M]   [ ][ ][ ]
-                #  [ ][ ][ ]   [X][ ][ ]
+                #  [ ][ ][ ]   [ ][ ][M]
+                #  [H][ ][M]   [ ][ ][ ]
+                #  [ ][ ][ ]   [H][ ][ ]
                 #
+                if distance == 2 or distance == 3:
+                    enemiesInRange.append(enemy)
                 if distance == 4 or distance == 6:
-                    mpoint = Point( (enemy.point.x + self.point.x)/2, ( enemy.point.x + self.point.x)/2 )
-                    if mpoint.isHit(things):
-                        return False
+                    mpoint = self.point.avg(enemy.point)
+                    if not mpoint.isHit(things):
+                        enemiesInRange.append(enemy)
                 #
                 #  [ ][ ][M]
                 #  [X][ ][ ]
@@ -43,8 +48,13 @@ class Character:
                 if distance == 5:
                     pointfloot = self.point.avgFloor(enemy.point)
                     pointCeil = self.point.avgCeil(enemy.point)
-                    if not pointfloot.isHit(things) or  not pointCeil.isHit(things):
-                        return False
+                    if not pointfloot.isHit(things) or not pointCeil.isHit(things):
+                        enemiesInRange.append(enemy)
 
+        return enemiesInRange
 
-        return  True
+    def getX(self):
+        return self.point.x
+
+    def getY(self):
+        return self.point.y
