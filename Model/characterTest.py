155e57c4d694afa36ecf84635610be5c77c5286a
Lars Charmer Jørgensen <Lars.charmer@gmail.com>
2022-11-06 13:49:57 +0100

    add test class

--- Model/characterTest.py
+++ Model/characterTest.py
@@ -1,8 +1,29 @@
 import unittest
 
+from  hero import Hero
+from levelMaker import Level_maker
+from point import Point
+
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+
+        lm = Level_maker()
+        hero = Hero()
+        monsters = lm.getMonsters(1)
+        self.assertEqual(len(monsters), 2)  # add assertion here
+
+        pillars = lm.getPillars(1)
+        for pillar in pillars:
+            print(pillar)
+
+        self.assertFalse(hero.canAttack(pillars, monsters))  # add assertion here
+
+        print(monsters[0])
+        monsters[0].point = Point(1,1)
+        self.assertTrue(hero.canAttack(pillars, monsters))  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
