0d72673636460919f75f2797dbf699c68eaddd48
Lars Charmer Jørgensen <Lars.charmer@gmail.com>
2022-11-12 12:37:59 +0100

    Can atttack moved

--- character.py
+++ character.py
@@ -25,45 +25,13 @@ class Character:
           "\n-------------------------------"
 
     def __str__(self):
-          return self.name + "(lif: " + str(self.life) + \
+          return  "(lif: " + str(self.life) + \
             ",str: " + str(self.strength) + \
             ",def: " + str(self.defence) + \
             ",mov: " + str(self.move) + \
                  ",ran: " + str(self.range) + \
-                 ")"
+                 ")" + self.name
 
-    def canAttack(self, pillars, enemies):
-        enemiesInRange = []
-        things = pillars + []
-        for enemy in enemies:
-            things.append(enemy.point)
-
-        # is enemy in range
-        for enemy in enemies:
-            distance = self.point.distance(enemy.point)
-            if distance > 0 and distance <= self.range:
-                #
-                #  [ ][ ][ ]   [ ][ ][M]
-                #  [H][ ][M]   [ ][ ][ ]
-                #  [ ][ ][ ]   [H][ ][ ]
-                #
-                if distance == 2 or distance == 3:
-                    enemiesInRange.append(enemy)
-                if distance == 4 or distance == 6:
-                    mpoint = self.point.avg(enemy.point)
-                    if not mpoint.isHit(things):
-                        enemiesInRange.append(enemy)
-                #
-                #  [ ][ ][M]
-                #  [X][ ][ ]
-                #
-                if distance == 5:
-                    pointfloot = self.point.avgFloor(enemy.point)
-                    pointCeil = self.point.avgCeil(enemy.point)
-                    if not pointfloot.isHit(things) or not pointCeil.isHit(things):
-                        enemiesInRange.append(enemy)
-
-        return enemiesInRange
 
     def getX(self):
         return self.point.x
